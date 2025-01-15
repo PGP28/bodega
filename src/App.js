1c03cbc1b2386e39d435b0ddd9a9f9151e192db3
Paul Guzmán <pguzman@bcoord.cl>
2025-01-15 10:43:26 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from "react";
+import AppRoutes from './AppRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <AppRoutes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
